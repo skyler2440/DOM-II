9006a45b78cb208ea21e5a05ff21ae7717c77f3e
Skyler Dowdy <skyler2440@gmail.com>
2019-04-09 15:57:00 -0400

    Added mousein mouseout

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+const linksin = document.querySelectorAll('a');
+for (var i=0; i < linksin.length; i++) {
+    linksin[i].addEventListener('mouseover', (event) => {
+        event.target.style.color = 'blue'
+        // console.log("target:", event.target);
+        });
+}
+const linksout = document.querySelectorAll('a');
+for (var i=0; i < linksout.length; i++) {
+    linksout[i].addEventListener('mouseout', (event) => {
+        event.target.style.color = 'black'
+        // console.log("target:", event.target);
+    });}
