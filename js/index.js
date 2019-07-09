bef7c0ec1028c7978e5d468052ee3ecd7f915fbb
skyler dowdy <skylerwebdev@gmail.com>
2019-07-09 15:08:02 -0400

    mouseenter mouseleave keydown keyup

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-// Your code goes here
+//MouseEnter MouseLeave
+var aLinks = document.querySelectorAll('a');
+var mouseEnters = function() { event.target.style.color = 'red' }
+var mouseLeaves = function() { event.target.style.color = '' }
+for (var i = aLinks.length - 1; i >= 0; --i) {
+    aLinks[i].onmouseenter = mouseEnters;
+    aLinks[i].onmouseleave = mouseLeaves;
+}
+
+//KeyDown KeyUp
+logoHeading = document.querySelector('.logo-heading');
+document.addEventListener('keydown', (event) => { logoHeading.textContent = 'Hello' });
+document.addEventListener('keyup', (event) => { logoHeading.textContent = 'Fun Bus' });
+
+document.querySelector('')
