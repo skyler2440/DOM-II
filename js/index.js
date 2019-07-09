18bbfc9412af28879cfe992750d9f37bc3dbb2f1
skyler dowdy <skylerwebdev@gmail.com>
2019-07-09 15:30:58 -0400

    click dblclick mouseover load

--- js/index.js
+++ js/index.js
@@ -12,4 +12,26 @@ logoHeading = document.querySelector('.logo-heading');
 document.addEventListener('keydown', (event) => { logoHeading.textContent = 'Hello' });
 document.addEventListener('keyup', (event) => { logoHeading.textContent = 'Fun Bus' });
 
-document.querySelector('')
+
+const button1 = document.querySelectorAll('.destination .btn')[0]
+const button2 = document.querySelectorAll('.destination .btn')[1]
+const button3 = document.querySelectorAll('.destination .btn')[2]
+
+button1.addEventListener('click', (event) => {
+    button1.style.background = 'purple'
+    button1.innerHTML = 'Pop!'
+
+})
+
+button2.addEventListener('dblclick', (event) => {
+    button2.style.background = 'blue'
+    button2.innerHTML = 'Double Click!'
+})
+
+button3.addEventListener('mouseover', (event) => {
+    button3.style.background = 'green'
+    button3.innerHTML = 'Green Version'
+})
+window.addEventListener('load', (event) => {
+    alert("The Page Loaded");
+});
